50374ebeced346015307ddc973a5268af05ea9d6
ivnNear <lolwto.gg@gmail.com>
2023-03-15 19:23:06 +0100

    loading added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import GifLoader from 'react-gif-loader';
 import styles from './styles.scss';
 import Header from './components/Header';
 import { Modal } from './components/Modal';
@@ -5,32 +6,42 @@ import { useEffect, useState } from 'react';
 import { Card } from './components/Card';
 import axios from 'axios';
 
-const arr = [
-  { title: 'New Balance version 0.5', price: 299, img: './imgItems/1.jpg' },
-  { title: 'New Balance GOAT Y', price: 499, img: './imgItems/2.jpg' },
-  { title: 'Adidas Ozweego EE6462', price: 149, img: './imgItems/3.jpg' },
-  {
-    title: 'Adidas Eastrail 2 GZ3016 Focus Olive',
-    price: 220,
-    img: './imgItems/4.jpg',
-  },
-];
+
+// imageSrc="https://media.giphy.com/media/l378zKVk7Eh3yHoJi/source.gif"
+// const arr = [
+//   { title: 'New Balance version 0.5', price: 299, img: './imgItems/1.jpg' },
+//   { title: 'New Balance GOAT Y', price: 499, img: './imgItems/2.jpg' },
+//   { title: 'Adidas Ozweego EE6462', price: 149, img: './imgItems/3.jpg' },
+//   {
+//     title: 'Adidas Eastrail 2 GZ3016 Focus Olive',
+//     price: 220,
+//     img: './imgItems/4.jpg',
+//   },
+// ];
 
 function App() {
   const [modal, setModal] = useState(false);
   const [inp, setInp] = useState('');
   const [items, setItems] = useState([]);
   const [cartItems, setCartItems] = useState([]);
-  const [removeCartItems, setRemoveCartItems] = useState([]);
+  const [load, setLoad] = useState(true);
+  const [error, setError] = useState(false);
+ 
 
 
   useEffect(() => {
     axios.get('https://6410743545a5f98532470365.mockapi.io/items').then((res) => setItems(res.data))
     axios.get('https://6410743545a5f98532470365.mockapi.io/cart').then((res) => setCartItems(res.data))
+    .finally(() => setLoad(false))
+
+    
+
+
    
     
 
   },[])
+
 
   const addToCart = (p) => {
     axios.post('https://6410743545a5f98532470365.mockapi.io/cart', p)
@@ -47,7 +58,14 @@ function App() {
 
   return (
     <div className='wrapper'>
-      <div className='container'>
+      
+      
+      {load ? <GifLoader
+                loading={true}
+                imageSrc="./load.gif"
+                // imageStyle={imageStyle}
+                overlayBackground="rgba(0,0,0,0.0)"
+            />  : <div className='container'>
         {modal && <Modal x={setModal} items={cartItems} removeFromCart={removeFromCart}/>}
         <Header x={setModal} />
 
@@ -75,7 +93,8 @@ function App() {
             })}
           </div>
         </main>
-      </div>
+      </div> }
+      
     </div>
   );
 }
